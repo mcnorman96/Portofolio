05171ff368742f25f2be680d5c4aa575be867643
Marcus Soerensen <marcus.sorensen@24i.com>
2023-09-19 10:17:20 +0200

    Added extra project images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,32 +4,36 @@ import Portpic from '../images/picportofolio.jpg';
 const About = () => {
   return (
     <div id="about" className="about">
-        <div className="row">
-          <div className="card">
-            <h2>About me</h2>
-            <div className="contactdiff">
-              <div className="col-md-6">
-                <p>Hello! <br/><br/>
-                My name is Marcus Norman, and I'm a web developer. I'm a graduated student from Web-development Zealand Roskilde in January 2021 and before that I went to Multimediadesigner in Slagelse.
-                  <br/><br/>
-                  I’m a very driven person, and I love to learn and evolve my skills as well learning new technologies. My primary focus has been on learning JS frameworks. For the most part it has been React application. I have worked a lot with frontend development, so I’m really experienced with HTML, CSS, and JavaScript. But I have also worked quite a bit with the backend development. I've created API's with PHP in laravel and also with NodeJS. Furthermore I have worked a bit with JWT for making more secure applications. 
-                  <br/><br/>
-                  I’m a very positive person and is quite outgoing and I’m always up for a challenge. I find it exciting to tackle new problems and the process of learning by doing. I'm good at working in teams as well as working independently on an assignment. I can easily adjust myself into working in different workflows and get along well with new people.
-                  </p>
-              </div>
-              <div className="col-md-6">
-                <div className="aboutimg">
-                  <img
-                    className="img-responsive"
-                    src={Portpic}
-                    alt="profilepic"
-                  />
-                </div>
+      <div className="row">
+        <div className="card">
+          <h2>About me</h2>
+          <div className="contactdiff">
+            <div className="col-md-6">
+              <p>
+                Hi there!
+                <br /><br />
+                I am Marcus Norman. A frontend developer always looking for more ways to improve. I have been a big fan of JavaScript and been very fond of developing application in React.js as well as using the frameworks like Next.js for some websites as well.
+                <br /><br />
+                I am a graduated student from web-development Zealand Roskilde back in 2021 and before that I took Multimediadesigner in Slagelse. So I both have some knowledge of SEO as well as design patterns for making an UI.
+                <br /><br />
+                I’m a very driven person, and I love to learn and evolve my skills as well learning new technologies. As I mentioned before my primary focus has been on developing in React.js, but I also have taking some time to both learn a bit of Node.js, PHP and use most of the used css frameworks like Tailwind, Bootstrap and foundatian
+                <br /><br />
+                As a person I'm usually really positive and tend to get along with all kinds of people. I like when I get a bit challenged in my everyday life. So I like when I get handed things that are new for me, so I can spend some time. I'm good at working in teams as well as working independently on an assignments, but I do love a work environment where there is open communication and people can share there thought and opionions so we can help eachother to get better.
+              </p>
+            </div>
+            <div className="col-md-6">
+              <div className="aboutimg">
+                <img
+                  className="img-responsive"
+                  src={Portpic}
+                  alt="profilepic"
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
