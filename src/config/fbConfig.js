70856170f602734075f552d5d2e63a627d6d7142
MarcusNorman24i <marcus.sorensen@24i.com>
2025-03-02 15:49:08 +0100

    Updated the way we are setting up firebase

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,21 +2,19 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
+import { apiKey, appId, authDomain, databaseURL, messagingSenderId, projectId, storageBucket } from "./fbConfigContants";
 
 // Replace this with your own config details
 var config = {
-  apiKey: "AIzaSyB4VwDKW0vXb_omUE7JdP6JfEkSzhJ4vjE",
-  authDomain: "movieprototype.firebaseapp.com",
-  databaseURL: "https://movieprototype.firebaseio.com",
-  projectId: "movieprototype",
-  storageBucket: "movieprototype.appspot.com",
-  messagingSenderId: "496661360704",
-  appId: "1:496661360704:web:199c913c73668ef51c6fa5",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
-// Initialize Firebase
 
 firebase.initializeApp(config);
-
-//firebase.firestore().settings({ timestampsInSnapshots: true })
 
 export default firebase;
